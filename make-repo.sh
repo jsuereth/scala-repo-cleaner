b4efeae76ee628847e36f70ffca6edbb248b41fe
Josh Suereth <joshua.suereth@gmail.com>
2011-10-14 10:40:55 -0400

    remove svn metadata from import

--- make-repo.sh
+++ make-repo.sh
@@ -4,5 +4,5 @@ authorfile="$(dirname $0)/svn-authors.txt"
 repourl="http://lampsvn.epfl.ch/svn-repos/scala/scala"
 git2svn="/var/lib/gems/1.8/bin/svn2git"
 
-#$git2svn $repourl --authors $authorfile --trunk trunk --tags tags --branches branches -m
-git svn clone -s $repourl --authors-file=$authorfile scala-svn
+#$git2svn $repourl --authors $authorfile --trunk trunk --tags tags --branches branches 
+git svn clone -s $repourl --authors-file=$authorfile scala-svn --no-metadata
