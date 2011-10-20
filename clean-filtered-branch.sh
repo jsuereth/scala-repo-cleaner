ef841a795de9be4c1e465e427b9b4118add03ca7
Josh Suereth <joshua.suereth@gmail.com>
2011-10-20 10:19:49 -0400

    More attempted fixes and cleanups.

--- clean-filtered-branch.sh
+++ clean-filtered-branch.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Cleans up git filter-branch mess.
-rm -rf .git/refs/original/ &&
+# rm -rf .git/refs/original/ &&
+
+git for-each-ref --format="%(refname)" refs/original/ | xargs -n 1 git update-ref -d &&
 git reflog expire --expire=now --all &&
 git gc --prune=now &&
 git gc --aggressive --prune=now
